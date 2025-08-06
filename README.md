4893a0e6627bd38c8a93f77a62bafbf2351f56d2
Melkamu Woldemariam <melkamu2@yahoo.com>
2025-08-05 21:42:23 -0400

    Refactoring the Old Code Done

--- README.md
+++ README.md
@@ -3,10 +3,32 @@ We will need to create two different data processing algorithms and will involve
 1. Transforming raw data into a formatted array of objects
 2. Using the output from the first step to provide a textual report on the data. 
 
-Objectives
-Use arrays to store ordered lists of data.
-Use objects to store keyed lists of data.
-Use conditional logic to process data.
-Use loops to handle repetitive tasks. 
-Transform data according to specifications.
+```Objectives```
+- Use arrays to store ordered lists of data.
+- Use objects to store keyed lists of data.
+- Use conditional logic to process data.
+- Use loops to handle repetitive tasks. 
+- Transform data according to specifications.
 
+```Part 1: Refractoring Old Code```
+Instruction: Revist your code from ALAB 308.3.1.
+
+```Part 2: Expanding Functionality```
+Instructions:
+- Declare a variable that stores the number of columns in each row of data within the csv
+- Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data. 
+- Store your results in a two-dimensional array (each row should be its own array, with individual entries from each column. Each row should be stored in a parent array, with the heading row located at index 0).
+- cache this array in a variable for later use. 
+
+```Part 3: Transforming Data```
+In order to make the data more obvious, transform out rows into objects:
+- For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value's column. Convert these keys to all lowercase for consistency.
+- Store these objects in an array, in the order that they were originally listed.
+
+```Part 4: Sorting and Manipulating Data```
+1. Remove the last element from the sorted array
+2. Insert the given object at index 1
+3. Add the given object to the end of the array
+
+```Part 5: Full Circle```
+Transform the final set of data back into CSV format. 
--- script.js
+++ script.js
@@ -1,12 +1,58 @@
 // Starting String
 let csvStr = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26`;
 
+// //  Variables
+// let cell1 = "";
+// let cell2 = "";
+// let cell3 = "";
+// let cell4 = "";
+// let commas = 0; // Counter variable
+
+// //Loop through string, for loop to have access to the index
+// for (let i = 0; i < csvStr.length; i++) {
+//   if (csvStr[i] == ",") {
+//     // If comma
+//     commas++; // Add to comma counter
+//   } else if (csvStr[i] == "\n") {
+//     // If new line
+//     console.log(cell1, cell2, cell3, cell4); // print new line
+
+//     // Clear previous data for new row
+//     commas = 0;
+//     cell1 = "";
+//     cell2 = "";
+//     cell3 = "";
+//     cell4 = "";
+//   } else {
+//     // for all chars
+//     if (commas == 0) {
+//       // put in the appropriate cell, based on commas
+//       cell1 += csvStr[i];
+//     } else if (commas == 1) {
+//       cell2 += csvStr[i];
+//     } else if (commas == 2) {
+//       cell3 += csvStr[i];
+//     } else {
+//       cell4 += csvStr[i];
+//     }
+//   }
+
+//   // If last char in string print final cells
+//   if (csvStr.length - 1 == i) {
+//     console.log(cell1, cell2, cell3, cell4);
+//   }
+// }
+
+//  How can I tell when a cell ends? commas!!!
+
+// Part 1: Refactoring Old Code 
 //  Variables
 let cell1 = "";
 let cell2 = "";
 let cell3 = "";
 let cell4 = "";
 let commas = 0; // Counter variable
+let rows = [] // initialize an array to save the rows into
 
 // Loop through string, for loop to have access to the index
 for (let i = 0; i < csvStr.length; i++) {
@@ -15,8 +61,8 @@ for (let i = 0; i < csvStr.length; i++) {
     commas++; // Add to comma counter
   } else if (csvStr[i] == "\n") {
     // If new line
-    console.log(cell1, cell2, cell3, cell4); // print new line
-
+    let currentRow = [cell1, cell2, cell3, cell4]; // I would want to save the outcome into to an array than printing it
+    rows.push(currentRow); // At each iteration, this would push the current row into the array
     // Clear previous data for new row
     commas = 0;
     cell1 = "";
@@ -39,8 +85,12 @@ for (let i = 0; i < csvStr.length; i++) {
 
   // If last char in string print final cells
   if (csvStr.length - 1 == i) {
-    console.log(cell1, cell2, cell3, cell4);
+    let row_last = [cell1, cell2, cell3, cell4];
+    rows.push(row_last); // Again, this pushes the last row into the array
   }
-}
-
-//  How can I tell when a cell ends? commas!!!
+    
+  };
+// To display the table without the '' around each cell data, I would like to use the following (I googled this)
+  rows.forEach (row => {
+    console.log(row.join(" "));
+  })
